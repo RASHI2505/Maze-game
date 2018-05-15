69d96665765a9a950bfbab695fcdfad678739bb7
Eduardo Rodríguez <edro998@gmail.com>
2018-05-14 23:14:16 -0400

    Make some walls and goal

--- maze.js
+++ maze.js
@@ -17,6 +17,21 @@ let keyPressed = {
 	down: false
 }
 
+// Goal object
+const Goal = {
+	x: canvas.width - 120,
+	y: canvas.height - 110,
+	width: 100,
+	height: 100,
+	draw: function(){
+		c.beginPath();
+		c.rect(this.x, this.y, this.width, this.height);
+		c.fillStyle = "orange";
+		c.fill();
+		c.closePath();
+	}
+}
+
 // Ball object
 function Ball(x, y, dx, dy){
 	this.x = x;
@@ -64,12 +79,49 @@ function Ball(x, y, dx, dy){
 	}
 }
 
+// Wall object
+function Wall(x, y, width, height){
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+
+	this.draw = function(){
+		c.beginPath();
+		c.rect(this.x, this.y, this.width, this.height);
+		c.fillStyle = "black";
+		c.fill();
+		c.closePath();
+	}
+}
+
+// Create walls using the object
+let wall0 = new Wall(0, 100, canvas.width - 150, 5);
+let wall1 = new Wall(150, 200, canvas.width, 5);
+let wall2 = new Wall(150, 200, 5, 125);
+let wall3 = new Wall(250, canvas.height, 5, -125);
+let wall4 = new Wall(350, 200, 5, 125);
+let wall5 = new Wall(450, canvas.height, 5, -125);
+let wall6 = new Wall(550, 200, 5, 125);
+let wall7 = new Wall(650, canvas.height, 5, -125);
+
 // Create the ball using the object 
 let ball = new Ball(30, 30, 7, 7);
 
 function start(){
 	requestAnimationFrame(start);
 	c.clearRect(0, 0, innerWidth, innerHeight);
+
+	wall0.draw();
+	wall1.draw();
+	wall2.draw();
+	wall3.draw();
+	wall4.draw();
+	wall5.draw();
+	wall6.draw();
+	wall7.draw();
+
+	Goal.draw();
 
 	ball.move();
 }
